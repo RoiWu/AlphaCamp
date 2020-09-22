2e96558320bd0af67c6f4020a351cd9ed846a8e1
RoiWu <roih1h116@gmail.com>
2020-09-22 22:15:38 +0800

    Add css/js template & structure basic website ...

--- RestaurantList/README.md
+++ RestaurantList/README.md
@@ -2,3 +2,5 @@ This first Test...
 
 npm install express
 npm install -g nodemon
+
+powerful https://github.com/ShihTingJustin/FeedYourSoul
--- RestaurantList/app.js
+++ RestaurantList/app.js
@@ -1,11 +1,21 @@
-// Include express from node_modules and define server related variables
+// app.js
+// require packages used in the project
 const express = require('express')
 const app = express()
 const port = 3000
+const exphbs = require('express-handlebars')
+
+// setting template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// setting static files
+app.use(express.static('public'))
 
 // setting the route and corresponding response
 app.get('/', (req, res) => {
-  res.send(`This is my Web Apps`)
+  const cssfile = "index"
+  res.render('index', { cssfile })
 })
 
 // Listen the server when it started
