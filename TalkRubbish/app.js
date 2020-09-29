a512c4166768b1f3024b99c85743e764fe0e5fbe
RoiWu <roih1h116@gmail.com>
2020-09-30 00:07:08 +0800

    add blank skip

--- TalkRubbish/app.js
+++ TalkRubbish/app.js
@@ -10,10 +10,12 @@ const generateRubbish = require('./models/generateRubbish')
 const occupations = require('./database/occupation.json')
 
 const app = express()
+const hbs = exphbs.create({ defaultLayout: 'main', extname: '.hbs' });
 const port = 3000
 
 // setting template engine
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+//app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
 
@@ -24,22 +26,23 @@ app.get('/', (req, res) => {
   const choseOccupation = ""
   const cssfile = "index"
   const occupation = occupations.info
-  res.render('index', { cssfile , occupation , choseOccupation})
+  res.render('index', { cssfile, occupation, choseOccupation })
 })
 
 app.post('/', (req, res) => {
-  if (req.body.length === 0) {
+  if (Object.keys(req.body).length === 0) {
     res.redirect('/')
   } else {
-    const name    = occupations.info.find(job => job.occupation_en===req.body.occupation)
-    const rubbish = "身為一個"+name.occupation+"，"+generateRubbish(req.body)
+    const name = occupations.info.find(job => job.occupation_en === req.body.occupation)
+    const rubbish = "身為一個" + name.occupation + "，" + generateRubbish(req.body)
     const cssfile = "index"
     const occupation = occupations.info
     const choseOccupation = req.body.occupation
-    res.render('index', { cssfile , occupation , rubbish, choseOccupation})
+    res.render('index', { cssfile, occupation, rubbish, choseOccupation })
   }
 })
 
 app.listen(port, () => {
   console.log(`App is running on http://localhost:${port}`)
 })
+
