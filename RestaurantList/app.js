35f9ec6e93e6170951c6db6cadd13d38df9dfc26
RoiWu <roih1h116@gmail.com>
2020-10-05 23:40:05 +0800

    add routes/edit page/delete function

--- RestaurantList/app.js
+++ RestaurantList/app.js
@@ -1,11 +1,26 @@
 // app.js
 // require packages used in the project
 const express = require('express')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+const routes = require('./routes')
+
 const app = express()
 const port = 3000
-const exphbs = require('express-handlebars')
+
+// setting template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// setting static files
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method')) // API設定時帶上_method就會轉換為HTTP方法
+
 //const restaurantList = require('./restaurant.json')
-const Restaurant = require('./models/restaurants') // 載入 todo model
+//const Restaurant = require('./models/restaurants') // 載入 todo model
+
 
 const mongoose = require('mongoose') // 載入 mongoose
 mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
@@ -21,24 +36,9 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+app.use(routes)
 
-// setting template engine
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
-app.set('view engine', 'handlebars')
-
-// setting static files
-app.use(express.static('public'))
-
-// setting the route and corresponding response
-app.get('/', (req, res) => {
-  const cssfile = "index"
-  Restaurant.find() // 取出 Todo model 裡的所有資料
-    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
-    .then(restaurantList => res.render('index', { cssfile, restaurants: restaurantList })) // 將資料傳給 index 樣板
-    .catch(error => console.error(error)) // 錯誤處理
-  //res.render('index', { cssfile, restaurants: restaurantList.results })
-})
-
+/*
 app.get('/search', (req, res) => {
   const cssfile = "index"
   const keyword = req.query.keyword
@@ -53,21 +53,7 @@ app.get('/search', (req, res) => {
     }) // 將資料傳給 index 樣板
     .catch(error => console.error(error)) // 錯誤處理
 })
-
-app.get('/restaurants/:restaurant_id', (req, res) => {
-  const cssfile = "show"
-
-  Restaurant.find() // 取出 Todo model 裡的所有資料
-    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
-    .then(restaurantList => {
-      const restaurant = restaurantList.find(
-        restaurant => restaurant.id === Number(req.params.restaurant_id)
-      )
-      res.render('show', { cssfile, restaurant })
-    }) // 將資料傳給 index 樣板
-    .catch(error => console.error(error)) // 錯誤處理
-})
-
+*/
 // Listen the server when it started
 app.listen(port, () => {
   console.log(`Express is running on http://localhost:${port}`)
