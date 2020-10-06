bf7f4eed3d4dafa57e2cecaee5bb4b8b63b97457
tywu6 <h1h116@hotmail.com>
2020-10-06 10:56:29 +0800

    add creat new page function

--- RestaurantList/routes/modules/restaurants.js
+++ RestaurantList/routes/modules/restaurants.js
@@ -7,13 +7,19 @@ const Restaurant = require('../../models/restaurants')
 router.get('/new', (req, res) => {
   return res.render('new')
 })
-/*
+
 router.post('/', (req, res) => {
   const { name, name_en, category, image, location, phone, google_map, rating, description } = req.body
-  return Restaurant.create({ name, name_en, category, image, location, phone, google_map, rating, description })
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})*/
+  Restaurant.find()
+    .sort("-id")
+    .limit(1)
+    .exec((error,data) => {
+      const id = Number(data[0].id)+1
+      return Restaurant.create({ id, name, name_en, category, image, location, phone, google_map, rating, description })
+        .then(() => res.redirect('/'))
+        .catch(error => console.log(error))
+    })
+})
 
 //READ 瀏覽單一餐廳資訊
 router.get('/:id', (req, res) => {
