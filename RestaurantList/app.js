c7b55e7b0de277a66826a489f7d97045525c2ee5
RoiWu <roih1h116@gmail.com>
2020-10-22 22:11:21 +0800

    set log info in app.js

--- RestaurantList/app.js
+++ RestaurantList/app.js
@@ -15,10 +15,25 @@ const port = 3000
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+const myLogger = function (req, res, next) {
+  const start_time = Date.now()
+  const start_format = Date(Date.now())
+  res.on("finish", () => {
+    const finish_time = Date.now()
+    duration = finish_time - start_time
+    server_message = start_format +
+      " | " + req.method + " from " +
+      req.originalUrl + " | total time: " + duration + "ms"
+    console.log(server_message);
+  })
+  next()
+}
+
 // setting static files
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method')) // API設定時帶上_method就會轉換為HTTP方法
+app.use(myLogger)
 app.use(routes)
 
 // Listen the server when it started
